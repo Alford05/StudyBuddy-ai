1113a83d0ceb38a0c93da3d3ee8ebb3d0cfe128d
Oakley <dalford278@gmail.com>
2025-12-26 20:29:41 -0500

    0.0.5 updated view

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";  // Optional: You can add a global CSS file for styling
+import "./index.css";  
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
